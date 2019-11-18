d9805b676941df1eb8d434991bd677bf0a795793
Euan Riggans <euan@euanriggans.com>
2019-11-18 22:07:06 +0000

    Added features from Firefox addon:
    - Implemented how to handle non-url setting
    - Implemented search engine setting

--- app/js/bulkopen.js
+++ app/js/bulkopen.js
@@ -1,10 +1,8 @@
-// @todo Force user to have at least 1 second delay as 0 seconds delay causes issues
-
 const {
     shell
 } = require('electron');
 
-const version = "1.4.0"
+const version = "1.5.0"
 
 $(() => {
     upgradeToJSONFormatting();
@@ -157,15 +155,27 @@ function openList(list) {
  * @param tabCreationDelay  The delay between opening a new url
  */
 function linksIterator(i, strings, tabCreationDelay) {
+    let ignoreURL = false;
     strings[i] = strings[i].trim();
     if (strings[i] === '') {
         return;
     }
     let url = strings[i];
-    if (!isProbablyUrl(url)) {
-        url = 'http://www.google.com/search?q=' + encodeURI(url);
+    if (!isProbablyUrl(url) && getSetting('non_url_handler') === "searchForString") {
+        url = encodeSearchQuery(url);
+    } else if (!isProbablyUrl(url) && getSetting('non_url_handler') === "ignoreString") {
+        ignoreURL = true;
+    } else if (!isProbablyUrl(url) && getSetting('non_url_handler') === "attemptToExtractURL") {
+        const extractedString = extractURLFromString(url);
+        if (isProbablyUrl(extractedString)) {
+            url = extractedString;
+        } else {
+            ignoreURL = true;
+        }
     }
-    shell.openExternal(url);
+    if (!ignoreURL) {
+        shell.openExternal(url);
+    }
     i++;
     if (i < strings.length) {
         setTimeout(linksIterator, tabCreationDelay, i, strings, tabCreationDelay);
@@ -330,11 +340,46 @@ function getSetting(setting) {
                     return userSettings.custom_theme;
                 case "currently_opened_tabs_display":
                     return userSettings.currently_opened_tabs_display;
+                case "non_url_handler":
+                    return userSettings.non_url_handler;
+                case "search_engine":
+                    return userSettings.search_engine;
                 default:
                     break;
             }
         }
     }
+}
+
+/**
+ * Builds search engine query url from a string
+ * @param {*} string 
+ */
+function encodeSearchQuery(string) {
+    const setting = getSetting("search_engine");
+    if (setting === "googleEngine") {
+        string = 'http://www.google.com/search?q=' + encodeURI(string);
+    } else if (setting === "duckduckgoEngine") {
+        string = 'https://duckduckgo.com/?q=' + encodeURI(string);
+    } else if (setting === "bingEngine") {
+        string = 'https://www.bing.com/search?q=' + encodeURI(string);
+    }
+    return string;
+}
+
+/**
+ * Attempts to extract a url from a string
+ * @param {*} string 
+ */
+function extractURLFromString(string) {
+    const urlRegex = /(https?:\/\/[^ ]*)/;
+    let url;
+    if (string.match(urlRegex)) {
+        url = string.match(urlRegex)[1];
+    } else {
+        url = "noextractionsuccess";
+    }
+    return url;
 }
 
 /**
